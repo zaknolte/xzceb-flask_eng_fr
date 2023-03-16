674bf6460252abb40e3eab4afb0f4c4d21bdf21b
zaknolte <127704632+zaknolte@users.noreply.github.com>
2023-03-15 19:32:31 -0700

    Add null test

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -7,10 +7,12 @@ class TestTranslator(unittest.TestCase):
     def test_englishToFrench(self):
         self.assertEqual(english_to_french(""), "")
         self.assertEqual(english_to_french("Hello"), "Bonjour")
+        self.assertNotEqual(english_to_french("dog"), "Bonjour")
 
     def test_frenchToEnglish(self):
         self.assertEqual(french_to_english(""), "")
         self.assertEqual(french_to_english("Bonjour"), "Hello")
+        self.assertNotEqual(french_to_english("Bonjour"), "dog")
 
 
 if __name__ == '__main__':
